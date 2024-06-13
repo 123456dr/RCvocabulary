530c08a9b61a985d2bb7717943db76bdb6bcd4ae
RC <125175307+123456dr@users.noreply.github.com>
2024-06-13 23:26:54 +0800

    6/13: * 改成按下類別才顯示清單
    6/13:
    * 改成按下類別才顯示清單
    =>為了解決手機無法載入單字列表頁面(資料過多，當機)
    * 在自定義單字頁面新增刪除開關啟用鍵，雙重點擊(啟用+按刪除)減少用戶失誤永久刪除單字

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "db2da62337877b66ae7726ae83a80f51
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cb5dc354cc842ea4331c152c6e2738b6",
-"/": "cb5dc354cc842ea4331c152c6e2738b6",
-"main.dart.js": "e5362ac7c937fa8918822e5daeb79287",
+"index.html": "dcbea0b06eca48d61f1a89ca090b164a",
+"/": "dcbea0b06eca48d61f1a89ca090b164a",
+"main.dart.js": "c3b777b1109609c13bdf60983e36ca8f",
 "manifest.json": "59594c52905de98958558b35cab74999",
 "version.json": "d7d8e902b9f77558e5158445038f8a9d"};
 // The application shell files that are downloaded before a service worker can
