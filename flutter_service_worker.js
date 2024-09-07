9ed03123c78b5b2e33565d76ce90595d04600502
RC <125175307+123456dr@users.noreply.github.com>
2024-09-07 12:12:30 +0800

    9/7-2修正待學習單字資料轉換
    9/7-2修正待學習單字資料轉換，為達到待學習單字庫目的[提供用戶額外重複練習，也就是學會待學習資料庫後還需再一次從原始題庫完成答題才移入已學會資料庫]，修正按下切換待學習單字後[按鈕learned]後單字將從原題庫移出

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "14f8c19fa306d670dfd4e3f69e9850ad
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c4edfe932a92f8360f5781bca6fea451",
-"/": "c4edfe932a92f8360f5781bca6fea451",
-"main.dart.js": "48378672d40b0112e35391390bc32ede",
+"index.html": "5b946305ee11f2b65a08e04b1310aa72",
+"/": "5b946305ee11f2b65a08e04b1310aa72",
+"main.dart.js": "015d72548e29588dbb3ff2b99db60387",
 "manifest.json": "59594c52905de98958558b35cab74999",
 "version.json": "d7d8e902b9f77558e5158445038f8a9d"};
 // The application shell files that are downloaded before a service worker can
