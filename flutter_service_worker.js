e8697eef0676fe2ae0b26d4b23dbe8b43bcd05dc
RC <125175307+123456dr@users.noreply.github.com>
2024-10-16 14:30:20 +0800

    10/16大大大更新:backgroundcolor1/2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "14f8c19fa306d670dfd4e3f69e9850ad
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a9d5b372eccd5546b8cc270193a95321",
-"/": "a9d5b372eccd5546b8cc270193a95321",
-"main.dart.js": "a537f12cae713acfba69f6f529495454",
+"index.html": "c720cdd2634fb73a6a0f58e5279fa24f",
+"/": "c720cdd2634fb73a6a0f58e5279fa24f",
+"main.dart.js": "e73aa2eed7e507034b953e40990ab5e5",
 "manifest.json": "59594c52905de98958558b35cab74999",
 "version.json": "d7d8e902b9f77558e5158445038f8a9d"};
 // The application shell files that are downloaded before a service worker can
